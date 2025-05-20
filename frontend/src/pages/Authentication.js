757248386ea32d01ae68166b3434165ceb40f8bf
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-20 15:44:17 +0500

    user can now signup but full functionality remains

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,39 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  const userData = await request.formData();
+  const userDetails = {
+    email: userData.get("email"),
+    password: userData.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userDetails),
+  });
+
+  if (mode !== "login" || mode !== "signup") {
+    throw json({ message: "Unsupported mode" }, { status: 422 });
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Failed to authenticate" }, { status: 500 });
+  }
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  return redirect("/");
+}
